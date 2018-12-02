6718e486b0b433d87a1d2c4e2a5997528e1ee7e9
ll9 <llwierer@web.de>
2018-12-02 20:31:45 +0100

    Basic Build script

---
+++ TestCiApp/TestCiApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestCiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
