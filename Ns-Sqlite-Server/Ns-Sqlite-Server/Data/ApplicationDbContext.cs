bc130311ff5d72ecceabb2604df5da468789613a
ll9 <llwierer@web.de>
2018-12-03 20:09:51 +0100

    basic db context

---
+++ Ns-Sqlite-Server/Ns-Sqlite-Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NsSqliteServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NsSqliteServer.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Lichtpunkt> Lichtpunkt { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Lichtpunkt>()
+                .Property(lp => lp.Id).HasDefaultValueSql("hex(randomblob(16))");
+        }
+    }
+}
