20d2de5886ddd9af1492728f2cd2dcad3ff41af8
ll9 <llwierer@web.de>
2018-12-03 20:05:13 +0100

    Basic Model

---
+++ Ns-Sqlite-Server/Ns-Sqlite-Server/Models/Lichtpunkt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NsSqliteServer.Models
+{
+    public class Lichtpunkt
+    {
+        [Key]
+        public string Id { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public string Ort { get; set; }
+        public string Straße { get; set; }
+    }
+}
