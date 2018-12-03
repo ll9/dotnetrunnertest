a00a9390b5c667c7aa63d6f2352ea2800f0732d3
ll9 <llwierer@web.de>
2018-12-03 20:13:00 +0100

    created db

---
+++ Ns-Sqlite-Server/Ns-Sqlite-Server/Migrations/20181203191229_Init.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NsSqliteServer.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lichtpunkt",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false, defaultValueSql: "hex(randomblob(16))"),
+                    Longitude = table.Column<double>(nullable: false),
+                    Latitude = table.Column<double>(nullable: false),
+                    Ort = table.Column<string>(nullable: true),
+                    Straße = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lichtpunkt", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lichtpunkt");
+        }
+    }
+}
